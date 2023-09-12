6cb6b387964e59181bdd2c7466eea5b3204ce8f1
Nam Chu Minh <41958727+namchuminh@users.noreply.github.com>
2023-09-12 10:30:43 +0700

    Update database.php

--- database/database.php
+++ database/database.php
@@ -3,7 +3,7 @@
 include  __DIR__.'../../config/config.php';
 
 function connectDB(){
-	$conn = new mysqli(SERVERNAME, USERNAME, PASSWORD, DATABASE);
+	$conn = new mysqli(SERVERNAME, USERNAME, PASSWORD, DATABASE, PORT);
 	if ($conn->connect_error) {
 	 	return die("Ket noi database that bai: " . $conn->connect_error);
 	}

b70d509d3f3d1c8e8644d15f0629e0495fb31693
Nam Chu Minh <41958727+namchuminh@users.noreply.github.com>
2023-09-12 09:44:50 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -4,5 +4,5 @@ define("BASE_URL", "https://webbansach.azurewebsites.net/");
 define("SERVERNAME", "db4free.net");
 define("USERNAME", "namchuminh");
 define("PASSWORD", "12345678@");
-define("DATABASE", "php_fcrud");
+define("DATABASE", "php_crud");
 
